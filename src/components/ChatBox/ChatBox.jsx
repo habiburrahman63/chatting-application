cc783346d3c48c6ee143e1e87902960e97d0435d
habiburrahman63 <habiburrahman90763@gamil.com>
2025-12-04 16:05:20 +0600

    g

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import userProfile from "../../assets/userProfile.png";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { PiTriangleFill } from "react-icons/pi";
@@ -6,9 +6,47 @@ import { MdOutlineEmojiEmotions } from "react-icons/md";
 import { CiCamera } from "react-icons/ci";
 import { IoIosSend } from "react-icons/io";
 import { useSelector } from "react-redux";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
 
 const ChatBox = () => {
+  const db = getDatabase();
   const activeData = useSelector((state) => state?.activeChatInfo?.value);
+  const data = useSelector((selector) => selector.userInfo.value.user);
+  const [msg, setMsg] = useState("");
+  const [msgList, setMsgList] = useState([]);
+  const handleChatBox = () => {
+    console.log(msg);
+    set(
+      push(ref(db, "msgeee/")),
+
+      {
+        senderId: data.uid,
+        senderName: data.displayName,
+        receiverId: activeData.id,
+        receiverName: activeData.name,
+        message: msg,
+      }
+    );
+  };
+
+  useEffect(() => {
+    const messageRef = ref(db, "msgeee/");
+    onValue(messageRef, (snapshot) => {
+      const arr = [];
+      snapshot.forEach((item) => {
+        if (
+          (data.uid == item.val().senderId &&
+            activeData.id == item.val().receiverId) ||
+          (data.uid == item.val().receiverId &&
+            activeData.id == item.val().senderId)
+        )
+          arr.push(item.val());
+      });
+      setMsgList(arr);
+    });
+  }, []);
+
+  console.log(msgList);
 
   return (
     <div>
@@ -32,90 +70,38 @@ const ChatBox = () => {
           </div>
         </div>
 
-        <div className="pt-[57px] pb-[47px] border-b-[1px] border-black/25">
-          <div>
-            <div className="relative">
-              <h2 className="py-[13px] px-[52px] bg-[#F1F1F1] inline-block rounded-[10px]  text-[16px] font-medium text-black font-primary">
-                Hey There !
-              </h2>
-              <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
-                Today, 2:01pm
-              </p>
-              <div className="absolute bottom-[18px] left-[-10px]">
-                <PiTriangleFill className="text-[#F1F1F1] text-[30px]" />
+        <div className="pt-[57px] pb-[47px] border-b-[1px] border-black/25 overflow-y-scroll h-[600px]">
+          {msgList.map((item) =>
+            data.uid == item.senderId ? (
+              <div className="my-[29px] text-end">
+                <div className="relative">
+                  <h2 className="py-[13px] px-[52px] bg-black inline-block rounded-[10px]  text-[16px] font-medium text-white font-primary">
+                    {item.message}
+                  </h2>
+                  <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
+                    Today, 2:01pm
+                  </p>
+                  <div className="absolute bottom-[19.5px] right-[-10px]">
+                    <PiTriangleFill className="text-[#000000] text-[30px]" />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="my-[29px]">
-            <div className="relative">
-              <h2 className="py-[13px] px-[52px] bg-[#F1F1F1] inline-block rounded-[10px]  text-[16px] font-medium text-black font-primary">
-                How are you doing?
-              </h2>
-              <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
-                Today, 2:01pm
-              </p>
-              <div className="absolute bottom-[18px] left-[-10px]">
-                <PiTriangleFill className="text-[#F1F1F1] text-[30px]" />
+            ) : (
+              <div>
+                <div className="relative">
+                  <h2 className="py-[13px] px-[52px] bg-[#F1F1F1] inline-block rounded-[10px]  text-[16px] font-medium text-black font-primary">
+                    {item?.message}
+                  </h2>
+                  <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
+                    Today, 2:01pm
+                  </p>
+                  <div className="absolute bottom-[18px] left-[-10px]">
+                    <PiTriangleFill className="text-[#F1F1F1] text-[30px]" />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="my-[29px] text-end">
-            <div className="relative">
-              <h2 className="py-[13px] px-[52px] bg-black inline-block rounded-[10px]  text-[16px] font-medium text-white font-primary">
-                Hello...
-              </h2>
-              <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
-                Today, 2:01pm
-              </p>
-              <div className="absolute bottom-[19.5px] right-[-10px]">
-                <PiTriangleFill className="text-[#000000] text-[30px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="my-[29px] text-end">
-            <div className="relative">
-              <h2 className="py-[13px] px-[52px] bg-black inline-block rounded-[10px]  text-[16px] font-medium text-white font-primary">
-                I am good and hoew about you?
-              </h2>
-              <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
-                Today, 2:01pm
-              </p>
-              <div className="absolute bottom-[19.5px] right-[-10px]">
-                <PiTriangleFill className="text-[#000000] text-[30px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="my-[29px]">
-            <div className="relative">
-              <h2 className="py-[13px] px-[52px] bg-[#F1F1F1] inline-block rounded-[10px]  text-[16px] font-medium text-black font-primary">
-                I am doing well. Can we meet up tomorrow?
-              </h2>
-              <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
-                Today, 2:01pm
-              </p>
-              <div className="absolute bottom-[18px] left-[-10px]">
-                <PiTriangleFill className="text-[#F1F1F1] text-[30px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="my-[29px] text-end">
-            <div className="relative">
-              <h2 className="py-[13px] px-[52px] bg-black inline-block rounded-[10px]  text-[16px] font-medium text-white font-primary">
-                Sure!
-              </h2>
-              <p className="text-[12px] font-normal text-black/25 font-primary mt-[5px]">
-                Today, 2:01pm
-              </p>
-              <div className="absolute bottom-[19.5px] right-[-10px]">
-                <PiTriangleFill className="text-[#000000] text-[30px]" />
-              </div>
-            </div>
-          </div>
+            )
+          )}
         </div>
 
         <div className="py-[35px]">
@@ -123,6 +109,9 @@ const ChatBox = () => {
             <div className="w-[537px] bg-[#F1F1F1] rounded-[10px] flex items-center justify-between px-[15px]">
               <div>
                 <input
+                  onChange={(e) => {
+                    setMsg(e.target.value);
+                  }}
                   type="text"
                   placeholder="massege"
                   className="w-full py-[15px]  text-[20px] font-primary outline-0"
@@ -134,7 +123,10 @@ const ChatBox = () => {
               </div>
             </div>
             <div className="bg-black p-[15px] rounded-[10px] ml-[20px]">
-              <IoIosSend className="text-[30px] text-white" />
+              <IoIosSend
+                onClick={handleChatBox}
+                className="text-[30px] text-white"
+              />
             </div>
           </div>
         </div>
